83820f3fbb3d846966eab25403286d8424a693e4
ImPatrik <Patrik.Rak94@gmail.com>
2025-11-08 12:13:04 +1000

    tailwind working

--- frontend/src/Ticket.js
+++ frontend/src/Ticket.js
@@ -1,7 +1,7 @@
 function Ticket() {
 return (
-        <div style={{display:"flex", flexWrap:"wrap"}}>
-            <form>
+        <div>
+            <form className="bg-blue-500">
                 <label>
                     Title:
                     <input name="titleInput" />
@@ -11,6 +11,13 @@ return (
                     <textarea name="descInput" rows={4} cols={40}/>
                 </label>
                 <button>Create Ticket</button>
+
+                <button 
+                    type="submit"
+                    className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md transition-colors"
+                >
+                    Create Ticket
+                </button>
             </form>
         </div>
     );
